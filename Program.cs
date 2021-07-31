cadd7d9036e0660ecd50b7078994f979c9ea1f74
Tuan <tuananhvd1998@gmail.com>
2021-07-31 18:41:45 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace new1
+{
+    class Program
+    {
+        class datamodel{
+            public string name{get; set;}
+        }
+        static void Main(string[] args)
+        {
+            StreamReader r = new StreamReader("./data.json");
+            string jsonString = r.ReadToEnd();
+            datamodel data = JsonConvert.DeserializeObject<datamodel>(jsonString);
+
+            Console.WriteLine("Tên của bạn là gì?");
+            string name = Console.ReadLine();
+            if(name == data.name){
+                string text = System.IO.File.ReadAllText("./text.txt");
+                Console.WriteLine(text);
+            } else {
+                Console.WriteLine("Món quà này không dành cho bạn !!");
+            }
+            Console.ReadKey();
+        }
+
+        public class SchemaInfo
+        {
+            public string name { get; set; }
+        }
+
+    }
+}
